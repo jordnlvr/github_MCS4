593de493ec7c39073598ea8129954e6c3746a89e
Kelly Seale <kellydseale@msn.com>
2014-10-24 23:08:36 +0000

    Changes made by Kelly Seale (kellydseale@msn.com).

--- MCS4/js/analytics.js
+++ MCS4/js/analytics.js
@@ -2,6 +2,17 @@
 
 	var productId = "5fa752ae121249ce9dfbb413aaadab1a"; // App unique product key
 
+    
+    function log(msg) {
+		var height = 0;
+
+        $console.append("<p style='color: white;'>" + msg + "</p>");
+		$console.children().each(function () {
+			height = height + $(this).outerHeight();
+		});
+		$console.scrollTop(height);
+	}
+    
 	// Make analytics available via the window.analytics variable
 	// Start analytics by calling window.analytics.Start()
 	var analytics = g.analytics = g.analytics || {};
@@ -11,24 +22,30 @@
 		var monitor = window.plugins.EqatecAnalytics.Monitor;
 		// Create the monitor instance using the unique product key for Analytics
 		var settings = factory.CreateSettings(productId);
+        var $console;
+        $console = $("#console"); //This only works if you have a div or other html element with the id console
 		settings.LoggingInterface = factory.CreateTraceLogger();
 		factory.CreateMonitorWithSettings(settings,
-		  function () {
-		  	console.log("Monitor created");
-		  	// Start the monitor inside the success-callback
-		  	monitor.Start(function () {
-		  		console.log("Monitor started");
-		  	});
-		  },
-		  function (msg) {
-		  	console.log("Error creating monitor: " + msg);
-		  });
-	}
+			function () {
+				console.log("Monitor created");
+                log("Monitor Created");
+				// Start the monitor inside the success-callback
+				monitor.Start(function () {
+					console.log("Monitor started");
+                    log("Monitor Started");
+				});
+			},
+			function (msg) {
+				console.log("Error creating monitor: " + msg);
+                log("Error creating monitor: " + msg);
+			}
+		);
+	};
 	analytics.Stop = function () {
 		var monitor = window.plugins.EqatecAnalytics.Monitor;
 		monitor.Stop();
-	}
+	};
 	analytics.Monitor = function () {
 		return window.plugins.EqatecAnalytics.Monitor;
-	}
+	};
 })(window);
